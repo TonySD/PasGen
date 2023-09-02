79f808dec24883dab717e56fad4cebe417560fef
TonySD <ahtoxa.b@gmail.com>
2023-09-03 00:43:38 +0300

    Removed some unnecessary testing code from main file

--- src/main/java/com/pasgen/PasGen.java
+++ src/main/java/com/pasgen/PasGen.java
@@ -5,14 +5,6 @@ import main.java.com.pasgen.generators.UpperLowerCase;
 
 public class PasGen {
     public static void main(String[] args) {
-        String test;
-        NumberAddition generator = new NumberAddition("test", 5);
-        String wtf;
-        int counter = 0;
-        while (!generator.isOver()) {
-            wtf = generator.getNextVariation();
-            System.out.println(wtf);
-            System.out.println(counter++);
-        }
+
     }
 }
