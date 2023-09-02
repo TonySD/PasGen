884f5c756d96edaa88e50e1ab36ddee6283c3c00
TonySD <ahtoxa.b@gmail.com>
2023-09-03 00:57:10 +0300

    Added config. It will be used in work of program for parameters

---
+++ src/main/java/com/pasgen/utils/Config.java
@@ -0,0 +1,11 @@
+package main.java.com.pasgen.utils;
+
+public class Config {
+    /*
+    TODO: Different modes for program
+    Modes: ...
+     */
+    public static int mode = 0;
+    // Writing filename
+    public static String filename = "./wordlist.lst";
+}
