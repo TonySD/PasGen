c20789699af49a5583d34f855f0a0787bfdd7304
TonySD <ahtoxa.b@gmail.com>
2023-09-02 23:36:06 +0300

    Implemented UpperLowerCase generator It generates every possible variation of given word, using lower and upper case

---
+++ src/main/java/com/pasgen/generators/Generator.java
@@ -0,0 +1,11 @@
+package main.java.com.pasgen.generators;
+
+public abstract class Generator {
+    protected final String baseWord;
+
+    protected Generator(String baseWord) {
+        this.baseWord = baseWord;
+    }
+
+    public abstract String getNextVariation();
+}
---
+++ src/main/java/com/pasgen/generators/UpperLowerCase.java
@@ -0,0 +1,62 @@
+package main.java.com.pasgen.generators;
+
+public class UpperLowerCase extends Generator {
+    private int amountOfBits;
+    private final int finalInt;
+    private int bitArray = 0;
+
+    public UpperLowerCase(String word) {
+        super(word.toLowerCase());
+        amountOfBits = 0;
+        for (char c : baseWord.toCharArray()) {
+            if (isConsonant(c)) amountOfBits++;
+        }
+
+        finalInt = 1 << amountOfBits;
+    }
+
+    private boolean[] getBits() {
+        boolean[] bits = new boolean[amountOfBits];
+
+        for (int i = amountOfBits - 1; i >= 0; i--) {
+            bits[i] = (bitArray & (1 << i)) != 0;
+        }
+
+        return bits;
+    }
+
+    private boolean isConsonant(char c) {
+        return 'a' <= c && c <= 'z';
+    }
+
+    public boolean isOver() {
+        return bitArray >= finalInt;
+    }
+
+    @Override
+    public String getNextVariation() {
+        if (isOver()) return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean[] bits = getBits();
+        int consonantCounter = 0;
+        char[] characters = baseWord.toCharArray();
+
+        for (int i = 0; i < baseWord.length(); ++i) {
+            if (!isConsonant(characters[i])) {
+                stringBuilder.append(characters[i]);
+                continue;
+            }
+
+            if (bits[consonantCounter]) stringBuilder.append(
+                    Character.toUpperCase(characters[i])
+            );
+            else stringBuilder.append(characters[i]);
+
+            consonantCounter++;
+        }
+
+        bitArray++;
+        return stringBuilder.toString();
+    }
+
+}
